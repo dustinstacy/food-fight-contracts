c008b3a6f32e8d69f3cf0c87e399d62980e1bc46
Dustin Stacy <dustin.stacy@outlook.com>
2025-03-05 19:42:24 -0600

    add `InGameCurrency` core functions

--- packages/foundry/contracts/InGameCurrency.sol
+++ packages/foundry/contracts/InGameCurrency.sol
@@ -17,4 +17,27 @@ contract InGameCurrency is ERC20, Ownable {
     /// @notice Initializes the contract with the provided owner.
     /// @param initialOwner The address that will be set as the owner of the contract.
     constructor(address initialOwner) ERC20("InGameCurrency", "IGC") Ownable(initialOwner) { }
+
+    ///////////////////////////////////////////////////////////
+    ///                    CORE FUNCTIONS                   ///
+    ///////////////////////////////////////////////////////////
+
+    /// @notice Mints the specified amount of tokens to the provided address.
+    /// @param to The address that will receive the minted tokens.
+    /// @param amount The amount of tokens to mint.
+    /// @dev Need to implement pricing and payment logic.
+    function mint(address to, uint256 amount) external payable {
+        // Get the price of the token.
+        // Check if the msg.value is equal to the price of the token.
+        // If not, revert with an error.
+        // If the price is correct, mint the token.
+        _mint(to, amount);
+    }
+
+    /// @notice Burns the specified amount of tokens from the caller's balance.
+    /// @param value The amount of tokens to burn.
+    /// @dev Need to implement balance checking.
+    function burn(uint256 value) public virtual {
+        _burn(_msgSender(), value);
+    }
 }
