319a8223ab9856f1956d4286d370faf68118916b
Dustin Stacy <dustin.stacy@outlook.com>
2025-03-10 16:34:40 -0500

    add events to the `AssetSwap` and update `balances` logic

--- packages/foundry/contracts/AssetSwap.sol
+++ packages/foundry/contracts/AssetSwap.sol
@@ -8,9 +8,6 @@ contract AssetSwap {
     ///                      ERRORS                         ///
     ///////////////////////////////////////////////////////////
 
-    /// Emitted when the caller tries to create a proposal for an asset they do not own.
-    error AssetSwapAssetNotOwned(uint256 requiredAssetId);
-
     /// Emitted when the caller tries to approve a proposal that is not pending.
     error AssetSwapProposalNotPending(ProposalStatus status);
 
@@ -24,6 +21,28 @@ contract AssetSwap {
     error AssetSwapArraysLengthMismatch(uint256 tokenIdsLength, uint256 amountsLength);
 
     ///////////////////////////////////////////////////////////
+    ///                    EVENTS                           ///
+    ///////////////////////////////////////////////////////////
+
+    // Emitted when a new proposal is created
+    event ProposalCreated(uint256 proposalId);
+
+    // Emitted when a proposal is approved
+    event ProposalApproved(uint256 proposalId);
+
+    // Emitted when a proposal is rejected
+    event ProposalRejected(uint256 proposalId);
+
+    // Emitted when a proposal is canceled
+    event ProposalCanceled(uint256 proposalId);
+
+    // Emitted when assets are deposited into the contract
+    event AssetsDeposited(address owner, uint256[] tokenIds, uint256[] amounts);
+
+    // Emitted when assets are withdrawn from the contract
+    event AssetsWithdrawn(address owner, uint256[] tokenIds, uint256[] amounts);
+
+    ///////////////////////////////////////////////////////////
     ///                     ENUMS                           ///
     ///////////////////////////////////////////////////////////
 
@@ -31,7 +50,6 @@ contract AssetSwap {
         Pending,
         Approved,
         Rejected,
-        Executed,
         Canceled
     }
 
@@ -55,6 +73,7 @@ contract AssetSwap {
     mapping(address owner => mapping(uint256 tokenId => uint256 balance)) public balances;
 
     uint256 private proposalCount;
+
     IERC1155 private assetsContract;
 
     ///////////////////////////////////////////////////////////
@@ -73,16 +92,19 @@ contract AssetSwap {
     function createProposal(address owner2, uint256 asset1TokenId, uint256 asset2TokenId) external {
         // Check if the caller has any of the asset1 deposited
         if (balances[msg.sender][asset1TokenId] == 0) {
-            // Deposit the asset1
+            // Create empty arrays for the depositAssets function
             uint256[] memory tokenIds = new uint256[](1);
             uint256[] memory amounts = new uint256[](1);
 
+            // Store the asset1 token ID and amount
             tokenIds[0] = asset1TokenId;
             amounts[0] = 1;
 
+            // Deposit asset1 into the contract
             depositAssets(tokenIds, amounts);
         }
 
+        // Increment the proposal count
         proposalCount++;
 
         // Create the proposal and store it in the proposals mapping
@@ -95,7 +117,9 @@ contract AssetSwap {
         });
 
         // Update the user balances
-        balances[msg.sender][asset1TokenId] += 1;
+        balances[msg.sender][asset1TokenId] -= 1;
+
+        emit ProposalCreated(proposalCount);
     }
 
     function approveProposal(uint256 proposalId) external {
@@ -111,22 +135,26 @@ contract AssetSwap {
             revert AssetSwapNotOwner2(proposal.owner2, msg.sender);
         }
 
-        // Deposit asset2 into the contract
+        // Create empty arrays for the depositAssets function
         uint256[] memory tokenIds = new uint256[](1);
         uint256[] memory amounts = new uint256[](1);
 
+        // Store the asset2 token ID and amount
         tokenIds[0] = proposal.asset2TokenId;
         amounts[0] = 1;
 
+        // Deposit asset2 into the contract
         depositAssets(tokenIds, amounts);
 
         // Update the user balances
-        balances[proposal.owner1][proposal.asset1TokenId] -= 1;
+        balances[proposal.owner2][proposal.asset2TokenId] -= 1;
         balances[proposal.owner1][proposal.asset2TokenId] += 1;
         balances[proposal.owner2][proposal.asset1TokenId] += 1;
 
         // Update the proposal status
         proposal.status = ProposalStatus.Approved;
+
+        emit ProposalApproved(proposalId);
     }
 
     function rejectProposal(uint256 proposalId) external {
@@ -137,13 +165,18 @@ contract AssetSwap {
             revert AssetSwapProposalNotPending(proposal.status);
         }
 
-        // Check if the caller is the owner1
+        // Check if the caller is the owner2
         if (proposal.owner2 != msg.sender) {
             revert AssetSwapNotOwner2(proposal.owner2, msg.sender);
         }
 
         // Update the proposal status
         proposal.status = ProposalStatus.Rejected;
+
+        // Update the user balances
+        balances[proposal.owner1][proposal.asset1TokenId] += 1;
+
+        emit ProposalRejected(proposalId);
     }
 
     function cancelProposal(uint256 proposalId) external {
@@ -161,39 +194,56 @@ contract AssetSwap {
 
         // Update the proposal status
         proposal.status = ProposalStatus.Canceled;
+
+        // Update the user balances
+        balances[proposal.owner1][proposal.asset1TokenId] += 1;
+
+        emit ProposalCanceled(proposalId);
     }
 
     function withdrawAssets(uint256[] memory tokenIds, uint256[] memory amounts) external {
+        // Check if the token IDs and amounts arrays have the same length
         if (tokenIds.length != amounts.length) {
             revert AssetSwapArraysLengthMismatch(tokenIds.length, amounts.length);
         }
 
+        // Store the necessary variables for the safeBatchTransferFrom function
         uint256 length = tokenIds.length;
         address from = address(this);
         address to = msg.sender;
         bytes memory data = "";
 
+        // Transfer the assets to the caller
         assetsContract.safeBatchTransferFrom(from, to, tokenIds, amounts, data);
 
+        // Update the user balances
         for (uint256 i = 0; i < length; i++) {
             balances[to][tokenIds[i]] -= amounts[i];
         }
+
+        emit AssetsWithdrawn(to, tokenIds, amounts);
     }
 
     function depositAssets(uint256[] memory tokenIds, uint256[] memory amounts) public {
+        // Check if the token IDs and amounts arrays have the same length
         if (tokenIds.length != amounts.length) {
             revert AssetSwapArraysLengthMismatch(tokenIds.length, amounts.length);
         }
 
+        // Store the necessary variables for the safeBatchTransferFrom function
         uint256 length = tokenIds.length;
         address from = msg.sender;
         address to = address(this);
         bytes memory data = "";
 
+        // Transfer the assets to the contract
         assetsContract.safeBatchTransferFrom(from, to, tokenIds, amounts, data);
 
+        // Update the user balances
         for (uint256 i = 0; i < length; i++) {
             balances[from][tokenIds[i]] += amounts[i];
         }
+
+        emit AssetsDeposited(from, tokenIds, amounts);
     }
 }
