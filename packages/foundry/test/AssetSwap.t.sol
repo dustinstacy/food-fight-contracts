6ea05395a3dfa935b38cfedf8f4511348808b69d
Dustin Stacy <dustin.stacy@outlook.com>
2025-03-12 18:28:12 -0500

    add view function tests to `AssetSwap.t.sol1`

--- packages/foundry/test/AssetSwap.t.sol
+++ packages/foundry/test/AssetSwap.t.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import { Test, console2 } from "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
 import { IERC1155Errors } from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 import { AssetSwap } from "@contracts/AssetSwap.sol";
 import { AssetFactorySetAssetsHelper } from "./AssetFactory.t.sol";
@@ -110,38 +110,6 @@ contract AssetSwapCreateProposalHelper is AssetSwapSetupHelper {
         assertEq(ASSET_TWO_ID, proposal.asset2TokenId);
         uint256 proposalStatus = uint256(proposal.status);
         assertEq(proposalStatus, pendingStatus);
-    }
-}
-
-contract AssetSwapApprovedProposalHelper is AssetSwapCreateProposalHelper {
-    function setUp() public override {
-        super.setUp();
-        approveProposal();
-    }
-
-    function approveProposal() public {
-        vm.startPrank(user2);
-        factory.setApprovalForAll(address(swap), true);
-        swap.approveProposal(1);
-        vm.stopPrank();
-
-        // Validate data for inheriting tests
-        AssetSwap.Proposal memory proposal = swap.getProposal(1);
-        uint256 status = uint256(proposal.status);
-        assertEq(status, approvedStatus);
-
-        uint256 user1Asset1Balance = swap.getBalance(user1, ASSET_ONE_ID);
-        uint256 user1Asset2Balance = swap.getBalance(user1, ASSET_TWO_ID);
-        uint256 user2Asset1Balance = swap.getBalance(user2, ASSET_ONE_ID);
-        uint256 user2Asset2Balance = swap.getBalance(user2, ASSET_TWO_ID);
-
-        assertEq(user1Asset1Balance, 0);
-        assertEq(user1Asset2Balance, DEPOSIT_1);
-        assertEq(user2Asset1Balance, DEPOSIT_1);
-        assertEq(user2Asset2Balance, 0);
-
-        assertEq(MINT_10 - DEPOSIT_1, factory.balanceOf(user1, ASSET_ONE_ID));
-        assertEq(MINT_10 - DEPOSIT_1, factory.balanceOf(user2, ASSET_TWO_ID));
     }
 }
 
@@ -559,26 +527,265 @@ contract AssetSwapDepositAssetsTest is AssetSwapSetupHelper {
     }
 }
 
-contract AssetSwatWithdrawAssetsTest is AssetSwapApprovedProposalHelper { }
+contract AssetSwapWithdrawAssetsTest is AssetSwapCreateProposalHelper {
+    function test_withdrawAssetsAfterDepositing() public {
+        uint256[] memory tokenIds = new uint256[](1);
+        uint256[] memory amounts = new uint256[](1);
+
+        tokenIds[0] = ASSET_ONE_ID;
+        amounts[0] = DEPOSIT_1;
+
+        vm.startPrank(user1);
+        factory.setApprovalForAll(address(swap), true);
+        swap.depositAssets(tokenIds, amounts);
+        vm.stopPrank();
+
+        // Check that the user has deposited the correct amount of assets
+        uint256 user1AssetBalance = swap.getBalance(user1, ASSET_ONE_ID);
+        assertEq(user1AssetBalance, DEPOSIT_1);
+
+        vm.startPrank(user1);
+        swap.withdrawAssets(tokenIds, amounts);
+        vm.stopPrank();
+
+        // Check that the user's asset balance was updated
+        uint256 expectedUser1AssetBalance = 0;
+        uint256 actualUser1AssetBalance = swap.getBalance(user1, ASSET_ONE_ID);
+        assertEq(expectedUser1AssetBalance, actualUser1AssetBalance);
+
+        // Check the asset contract balance to ensure the assets were withdrawn
+        // Minusing 1 because this test suite is extending the created proposal test
+        uint256 expectedFactoryBalance = MINT_10 - DEPOSIT_1;
+        uint256 actualFactoryBalance = factory.balanceOf(user1, ASSET_ONE_ID);
+        assertEq(expectedFactoryBalance, actualFactoryBalance);
+    }
+
+    function test_withdrawMultipleAssetsAfterDepositing() public {
+        uint256[] memory tokenIds = new uint256[](3);
+        uint256[] memory amounts = new uint256[](3);
+
+        tokenIds[0] = ASSET_ONE_ID;
+        tokenIds[1] = ASSET_TWO_ID;
+        tokenIds[2] = ASSET_THREE_ID;
+
+        amounts[0] = DEPOSIT_1;
+        amounts[1] = DEPOSIT_5;
+        amounts[2] = DEPOSIT_10;
+
+        vm.startPrank(user1);
+        factory.setApprovalForAll(address(swap), true);
+        swap.depositAssets(tokenIds, amounts);
+        vm.stopPrank();
+
+        // Check that the user has deposited the correct amount of assets
+        uint256 user1Asset1Balance = swap.getBalance(user1, ASSET_ONE_ID);
+        uint256 user1Asset2Balance = swap.getBalance(user1, ASSET_TWO_ID);
+        uint256 user1Asset3Balance = swap.getBalance(user1, ASSET_THREE_ID);
+
+        assertEq(user1Asset1Balance, DEPOSIT_1);
+        assertEq(user1Asset2Balance, DEPOSIT_5);
+        assertEq(user1Asset3Balance, DEPOSIT_10);
+
+        vm.startPrank(user1);
+        swap.withdrawAssets(tokenIds, amounts);
+        vm.stopPrank();
+
+        // Check that the user's asset balance was updated
+        uint256 expectedUser1Asset1Balance = 0;
+        uint256 expectedUser1Asset2Balance = 0;
+        uint256 expectedUser1Asset3Balance = 0;
+
+        uint256 actualUser1Asset1Balance = swap.getBalance(user1, ASSET_ONE_ID);
+        uint256 actualUser1Asset2Balance = swap.getBalance(user1, ASSET_TWO_ID);
+        uint256 actualUser1Asset3Balance = swap.getBalance(user1, ASSET_THREE_ID);
+
+        assertEq(expectedUser1Asset1Balance, actualUser1Asset1Balance);
+        assertEq(expectedUser1Asset2Balance, actualUser1Asset2Balance);
+        assertEq(expectedUser1Asset3Balance, actualUser1Asset3Balance);
+
+        // Check the asset contract balance to ensure the assets were withdrawn
+        // Minusing 1 because this test suite is extending the created proposal test
+        uint256 expectedFactoryBalance1 = MINT_10 - DEPOSIT_1;
+        uint256 expectedFactoryBalance2 = MINT_10;
+        uint256 expectedFactoryBalance3 = MINT_10;
+
+        uint256 actualFactoryBalance1 = factory.balanceOf(user1, ASSET_ONE_ID);
+        uint256 actualFactoryBalance2 = factory.balanceOf(user1, ASSET_TWO_ID);
+        uint256 actualFactoryBalance3 = factory.balanceOf(user1, ASSET_THREE_ID);
+
+        assertEq(expectedFactoryBalance1, actualFactoryBalance1);
+        assertEq(expectedFactoryBalance2, actualFactoryBalance2);
+        assertEq(expectedFactoryBalance3, actualFactoryBalance3);
+    }
+
+    function test_withdrawAssetsAfterCancelingProposal() public {
+        vm.startPrank(user1);
+        swap.cancelProposal(1);
+        vm.stopPrank();
+
+        uint256[] memory tokenIds = new uint256[](1);
+        uint256[] memory amounts = new uint256[](1);
+
+        tokenIds[0] = ASSET_ONE_ID;
+        amounts[0] = DEPOSIT_1;
+
+        vm.startPrank(user1);
+        swap.withdrawAssets(tokenIds, amounts);
+        vm.stopPrank();
+
+        // Check that the user's asset balance was updated
+        uint256 expectedUser1AssetBalance = 0;
+        uint256 actualUser1AssetBalance = swap.getBalance(user1, ASSET_ONE_ID);
+        assertEq(expectedUser1AssetBalance, actualUser1AssetBalance);
+
+        // Check the asset contract balance to ensure the assets were not withdrawn
+        uint256 expectedFactoryBalance = MINT_10;
+        uint256 actualFactoryBalance = factory.balanceOf(user1, ASSET_ONE_ID);
+        assertEq(expectedFactoryBalance, actualFactoryBalance);
+    }
+
+    function test_withdrawAssetsAfterApproval() public {
+        vm.startPrank(user2);
+        factory.setApprovalForAll(address(swap), true);
+        swap.approveProposal(1);
+        vm.stopPrank();
+
+        uint256[] memory tokenIds = new uint256[](1);
+        uint256[] memory tokenIds2 = new uint256[](1);
+        uint256[] memory amounts = new uint256[](1);
+
+        tokenIds[0] = ASSET_ONE_ID;
+        tokenIds2[0] = ASSET_TWO_ID;
+        amounts[0] = DEPOSIT_1;
+
+        vm.prank(user1);
+        swap.withdrawAssets(tokenIds2, amounts);
+
+        vm.prank(user2);
+        swap.withdrawAssets(tokenIds, amounts);
+
+        // Check that the user's asset balance was updated
+        uint256 expectedUser1AssetBalance = 0;
+        uint256 actualUser1AssetBalance = swap.getBalance(user1, ASSET_ONE_ID);
+        assertEq(expectedUser1AssetBalance, actualUser1AssetBalance);
+
+        uint256 expectedUser2AssetBalance = 0;
+        uint256 actualUser2AssetBalance = swap.getBalance(user2, ASSET_TWO_ID);
+        assertEq(expectedUser2AssetBalance, actualUser2AssetBalance);
+
+        // Check the asset contract balance to ensure the assets were not withdrawn
+        uint256 expectedUser1Asset1FactoryBalance = MINT_10 - DEPOSIT_1;
+        uint256 expectedUser1Asset2FactoryBalance = MINT_10 + DEPOSIT_1;
+        uint256 expectedUser2Asset2FactoryBalance = MINT_10 - DEPOSIT_1;
+        uint256 expectedUser2Asset1FactoryBalance = MINT_10 + DEPOSIT_1;
+
+        uint256 actualUser1Asset1FactoryBalance = factory.balanceOf(user1, ASSET_ONE_ID);
+        uint256 actualUser1Asset2FactoryBalance = factory.balanceOf(user1, ASSET_TWO_ID);
+        uint256 actualUser2Asset2FactoryBalance = factory.balanceOf(user2, ASSET_TWO_ID);
+        uint256 actualUser2Asset1FactoryBalance = factory.balanceOf(user2, ASSET_ONE_ID);
+
+        assertEq(expectedUser1Asset1FactoryBalance, actualUser1Asset1FactoryBalance);
+        assertEq(expectedUser1Asset2FactoryBalance, actualUser1Asset2FactoryBalance);
+        assertEq(expectedUser2Asset2FactoryBalance, actualUser2Asset2FactoryBalance);
+        assertEq(expectedUser2Asset1FactoryBalance, actualUser2Asset1FactoryBalance);
+    }
+
+    function test_withdrawAssets_EmitEvent() public {
+        uint256[] memory tokenIds = new uint256[](1);
+        uint256[] memory amounts = new uint256[](1);
+
+        tokenIds[0] = ASSET_ONE_ID;
+        amounts[0] = DEPOSIT_1;
+
+        vm.startPrank(user1);
+        factory.setApprovalForAll(address(swap), true);
+        swap.depositAssets(tokenIds, amounts);
+        vm.expectEmit(false, false, false, false, address(swap));
+        emit AssetsWithdrawn(user1, tokenIds, amounts);
+        swap.withdrawAssets(tokenIds, amounts);
+        vm.stopPrank();
+    }
+
+    function test_withdrawAssets_RevertIf_ArraysNotSameLength() public {
+        uint256[] memory tokenIds = new uint256[](1);
+        uint256[] memory amounts = new uint256[](2);
+
+        tokenIds[0] = ASSET_ONE_ID;
+        amounts[0] = DEPOSIT_1;
+        amounts[1] = DEPOSIT_5;
+
+        vm.startPrank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(AssetSwap.AssetSwapArraysLengthMismatch.selector, tokenIds.length, amounts.length)
+        );
+        swap.withdrawAssets(tokenIds, amounts);
+        vm.stopPrank();
+    }
+
+    function test_withdrawAssets_RevertIf_InsufficientBalance() public {
+        uint256[] memory tokenIds = new uint256[](1);
+        uint256[] memory amounts = new uint256[](1);
+
+        tokenIds[0] = ASSET_THREE_ID;
+        amounts[0] = DEPOSIT_1;
+
+        vm.startPrank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(AssetSwap.AssetSwapInsufficientBalance.selector, user1, 0, amounts[0], tokenIds[0])
+        );
+        swap.withdrawAssets(tokenIds, amounts);
+        vm.stopPrank();
+    }
+}
 
 ///////////////////////////////////////////////////////////
 ///                  VIEW FUNCTION TESTS                ///
 ///////////////////////////////////////////////////////////
 
-contract AssetSwapViewFunctionsTest is AssetSwapSetupHelper {
-    function test_getProposal() public { }
+contract AssetSwapViewFunctionsTest is AssetSwapCreateProposalHelper {
+    function test_getProposal() public view {
+        AssetSwap.Proposal memory proposal = swap.getProposal(1);
 
-    function test_getProposalUser1() public { }
+        assertEq(user1, proposal.user1);
+        assertEq(user2, proposal.user2);
+        assertEq(ASSET_ONE_ID, proposal.asset1TokenId);
+        assertEq(ASSET_TWO_ID, proposal.asset2TokenId);
+        uint256 status = uint256(proposal.status);
+        assertEq(status, pendingStatus);
+    }
 
-    function test_getProposalUser2() public { }
+    function test_getProposalUser1() public view {
+        address user1Address = swap.getProposalUser1(1);
+        assertEq(user1, user1Address);
+    }
 
-    function test_getProposalAsset1TokenId() public { }
+    function test_getProposalUser2() public view {
+        address user2Address = swap.getProposalUser2(1);
+        assertEq(user2, user2Address);
+    }
 
-    function test_getProposalAsset2TokenId() public { }
+    function test_getProposalAsset1TokenId() public view {
+        uint256 asset1TokenId = swap.getProposalAsset1TokenId(1);
+        assertEq(ASSET_ONE_ID, asset1TokenId);
+    }
 
-    function test_getProposalStatus() public { }
+    function test_getProposalAsset2TokenId() public view {
+        uint256 asset2TokenId = swap.getProposalAsset2TokenId(1);
+        assertEq(ASSET_TWO_ID, asset2TokenId);
+    }
 
-    function test_getBalance() public { }
+    function test_getProposalStatus() public view {
+        uint256 status = uint256(swap.getProposalStatus(1));
+        assertEq(status, pendingStatus);
+    }
 
-    function test_getProposalCount() public { }
+    function test_getBalance() public view {
+        uint256 balance = swap.getBalance(user1, ASSET_ONE_ID);
+        assertEq(balance, 0);
+    }
+
+    function test_getProposalCount() public view {
+        uint256 proposalCount = swap.getProposalCount();
+        assertEq(proposalCount, 1);
+    }
 }
