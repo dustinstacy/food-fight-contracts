3930c6229d86cdee3d1f62093f2f5156b78be5f1
Dustin Stacy <dustin.stacy@outlook.com>
2025-03-16 18:23:27 -0500

    remove unnecessary imports

--- packages/foundry/contracts/AssetFactory.sol
+++ packages/foundry/contracts/AssetFactory.sol
@@ -1,8 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Remove when deploying to a live network.
-import "forge-std/console.sol";
+// Remove unnecessary imports
 
 import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import { IERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
