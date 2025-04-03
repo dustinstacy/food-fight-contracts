f2046aefb0bffc7eae8852e86f1c7cd48884437b
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-03 18:10:12 -0500

    update NATSPEC and comments - make `trade` contract instance an approved vault caller

--- packages/foundry/test/helpers/AssetTradeTestHelper.sol
+++ packages/foundry/test/helpers/AssetTradeTestHelper.sol
@@ -6,26 +6,45 @@ import { AssetTrade } from "@contracts/AssetTrade.sol";
 import { AssetFactoryTestHelper } from "./AssetFactoryTestHelper.sol";
 import { AssetVaultTestHelper } from "./AssetVaultTestHelper.sol";
 
-/// @dev Helper contract to setup the environment for AssetTrade testing and functions to interact with AssetTrade contract
-contract AssetTradeTestHelper is AssetFactoryTestHelper, AssetVaultTestHelper {
-    /// @dev Create an instance of the AssetTrade contract that all tests will use
+/// @dev Helper contract for interacting with the AssetTrade contract.
+/// Inheritance Tree:
+/// AssetTradeTestHelper -> AssetVaultTestHelper -> AssetFactoryTestHelper -> TestingVariables & Test
+contract AssetTradeTestHelper is AssetVaultTestHelper {
+    ////////////////////////////////////////////////
+    /// Contract Instance                        ///
+    ////////////////////////////////////////////////
+
+    /// @dev Instance of the AssetTrade contract
     AssetTrade trade = new AssetTrade(address(factory), address(vault));
+
+    ////////////////////////////////////////////////
+    /// Enums                                    ///
+    ////////////////////////////////////////////////
 
     /// @dev Convert the proposal status enum to integers for testing
     uint256 pendingStatus = uint256(AssetTrade.ProposalStatus.Pending);
-    uint256 approvedStatus = uint256(AssetTrade.ProposalStatus.Approved);
+    uint256 approvedStatus = uint256(AssetTrade.ProposalStatus.Accepted);
     uint256 rejectedStatus = uint256(AssetTrade.ProposalStatus.Rejected);
     uint256 canceledStatus = uint256(AssetTrade.ProposalStatus.Canceled);
 
+    ////////////////////////////////////////////////
+    /// Setup Function                           ///
+    ////////////////////////////////////////////////
+
     /// @dev Set up the initial state for the AssetTrade contract
-    // Mints IGC and Assets for user B, then deposits assets into the vault for user A and user B
-    // Also updates the starting vault balances for user A and user B
+    // Sets the asset data, mints IGC and assets for userA & userB, deposits single assets into the vault for trade,
+    // and stores the starting balances for userA & userB.
     function setUp() public virtual override {
+        // Calls the setup function from the parent class
         super.setUp();
         mintIGCTestHelper(userB, ONE_MILLION);
         mintAssetTestHelper(userB, all);
         depositAssetsTestHelper(userA, asset1Single, amountSingle);
         depositAssetsTestHelper(userB, asset2Single, amountSingle);
+
+        // Approve the AssetTrade contract to lock and unlock assets in the vault
+        vm.prank(owner);
+        vault.approveCaller(address(trade));
 
         userAStartingVaultAssetOneBalance = vault.balanceOf(userA, ASSET_ONE_ID);
         userAStartingVaultAssetTwoBalance = vault.balanceOf(userA, ASSET_TWO_ID);
@@ -33,15 +52,19 @@ contract AssetTradeTestHelper is AssetFactoryTestHelper, AssetVaultTestHelper {
         userBStartingVaultAssetTwoBalance = vault.balanceOf(userB, ASSET_TWO_ID);
     }
 
+    ////////////////////////////////////////////////
+    /// Contract Call Helpers                    ///
+    ////////////////////////////////////////////////
+
+    //!! Update this to be accept a receiver, assetToTrade, and assetToReceive
     /// @dev Create a proposal for user A to trade asset one for asset two with user B
-    //!! Update this to be more modular
     function createProposalHelper() public {
         vm.prank(userA);
         trade.createProposal(userB, ASSET_ONE_ID, ASSET_TWO_ID);
     }
 
-    /// @dev Cancel the proposal for user A
     //!! Update to accept a user and proposalId
+    /// @dev Cancel the proposal for user A
     function cancelProposalHelper() public {
         vm.prank(userA);
         trade.cancelProposal(1);
