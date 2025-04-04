ee93624819924c26447994d367aaa9837b328910
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-03 23:35:06 -0500

    add `AssetRental` contract

--- packages/foundry/contracts/AssetRental.sol
+++ packages/foundry/contracts/AssetRental.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+// restructured to match Solidity style guide - updated events - implemented functions
+
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import { IERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
@@ -10,44 +12,7 @@ import { AssetVault } from "./AssetVault.sol";
 /// @notice This contract allows users to list assets for rent and rent assets from other users.
 contract AssetRental is IERC1155Receiver {
     ///////////////////////////////////////////////////////////
-    ///                     ERRORS                          ///
-    ///////////////////////////////////////////////////////////
-
-    /// @notice Thrown when the asset is not available for rent.
-    error AssetRentalAssetIsNotAvailable(RentalStatus status);
-
-    /// @notice Thrown when the asset is currently being rented.
-    error AssetRentalIsCurrentlyRented(RentalStatus status);
-
-    /// @notice Thrown when the caller is not the owner of the asset.
-    error AssetRentalNotTheOwner(address caller, address owner);
-
-    /// @notice Thrown when the caller is not the renter of the asset.
-    error AssetRentalNotTheRenter(address caller, address renter);
-
-    ///////////////////////////////////////////////////////////
-    ///                     EVENTS                          ///
-    ///////////////////////////////////////////////////////////
-
-    /// @notice Emitted when an asset is posted for rent.
-    event RentalAssetPosted(address rentalOwner, uint256 assetId, uint256 rentalPrice, uint256 rentalDuration);
-
-    /// @notice Emitted when an asset is relisted.
-    event RentalAssetRelisted(address rentalOwner, uint256 rentalAssetId, uint256 rentalPrice, uint256 rentalDuration);
-
-    /// @notice Emitted when an asset is unlisted.
-    event RentalAssetUnlisted(address rentalOwner, uint256 rentalAssetId);
-
-    /// @notice Emitted when an asset is updated.
-    event RentalAssetUpdated(address rentalOwner, uint256 rentalAssetId, uint256 rentalPrice, uint256 rentalDuration);
-
-    /// @notice Emitted when an asset is rented.
-    event RentalAssetRented(
-        address renter, uint256 rentalAssetId, uint256 rentalPrice, uint256 rentalDuration, uint256 timeRented
-    );
-
-    ///////////////////////////////////////////////////////////
-    ///                     ENUMS                           ///
+    ///                   TYPE DECLARATIONS                 ///
     ///////////////////////////////////////////////////////////
 
     /// @notice The status of the rental asset.
@@ -56,10 +21,6 @@ contract AssetRental is IERC1155Receiver {
         Rented,
         Unavailable
     }
-
-    ///////////////////////////////////////////////////////////
-    ///                     STRUCTS                         ///
-    ///////////////////////////////////////////////////////////
 
     /// @notice The details of the rental asset.
     struct RentalAsset {
@@ -77,7 +38,7 @@ contract AssetRental is IERC1155Receiver {
     ///////////////////////////////////////////////////////////
 
     /// @notice Mapping of rental ID to rental asset.
-    mapping(uint256 rentalAssetId => RentalAsset) private rentals;
+    mapping(uint256 rentalAssetId => RentalAsset) private rentalAssets;
 
     /// @notice Mapping of a user to their rented asset balances.
     mapping(address user => mapping(uint256 assetId => uint256 balance)) private rentedAssets;
@@ -95,6 +56,44 @@ contract AssetRental is IERC1155Receiver {
     uint256 private rentalAssetCount;
 
     ///////////////////////////////////////////////////////////
+    ///                      EVENTS                         ///
+    ///////////////////////////////////////////////////////////
+
+    /// @notice Emitted when an asset is posted for rent.
+    event RentalAssetPosted(address rentalOwner, uint256 rentalAssetId);
+
+    /// @notice Emitted when an asset is relisted.
+    event RentalAssetRelisted(address rentalOwner, uint256 rentalAssetId);
+
+    /// @notice Emitted when an asset is unlisted.
+    event RentalAssetUnlisted(address rentalOwner, uint256 rentalAssetId);
+
+    /// @notice Emitted when an asset is updated.
+    event RentalAssetUpdated(address rentalOwner, uint256 rentalAssetId);
+
+    /// @notice Emitted when an asset is rented.
+    event RentalAssetRented(address renter, uint256 rentalAssetId, uint256 timeRented);
+
+    ///////////////////////////////////////////////////////////
+    ///                     ERRORS                          ///
+    ///////////////////////////////////////////////////////////
+
+    /// @notice Thrown when the asset is not available for rent.
+    error AssetRentalNotAvailable(RentalStatus status);
+
+    /// @notice Thrown when the asset is already available for rent.
+    error AssetRentalAlreadyAvailable(RentalStatus status);
+
+    /// @notice Thrown when the asset is not expired.
+    error AssetRentalNotExpired(uint256 expiration);
+
+    /// @notice Thrown when the caller is not the owner of the asset.
+    error AssetRentalNotTheOwner(address caller, address owner);
+
+    /// @notice Thrown when the caller is not the renter of the asset.
+    error AssetRentalNotTheRenter(address caller, address renter);
+
+    ///////////////////////////////////////////////////////////
     ///                     CONSTRUCTOR                     ///
     ///////////////////////////////////////////////////////////
 
@@ -107,7 +106,7 @@ contract AssetRental is IERC1155Receiver {
     }
 
     ///////////////////////////////////////////////////////////
-    ///                    CORE FUNCTIONS                   ///
+    ///                RENTAL OWNER FUNCTIONS               ///
     ///////////////////////////////////////////////////////////
 
     /// @notice Post an asset for rent.
@@ -118,7 +117,7 @@ contract AssetRental is IERC1155Receiver {
     //!! Consider bounds for price and duration.
     function createRental(uint256 assetId, uint256 price, uint256 duration) external {
         rentalAssetCount++;
-        rentals[rentalAssetCount] = RentalAsset({
+        rentalAssets[rentalAssetCount] = RentalAsset({
             owner: msg.sender,
             renter: address(0),
             assetId: assetId,
@@ -130,31 +129,85 @@ contract AssetRental is IERC1155Receiver {
 
         vault.lockAsset(msg.sender, assetId, 1);
 
-        emit RentalAssetPosted(msg.sender, assetId, price, duration);
+        emit RentalAssetPosted(msg.sender, rentalAssetCount);
     }
 
-    function relistAsset(uint256 rentalAssetId) external {
-        RentalAsset memory rental = rentals[rentalAssetId];
+    function unlistRental(uint256 rentalAssetId) external {
+        RentalAsset storage rental = rentalAssets[rentalAssetId];
 
-        emit RentalAssetRelisted(msg.sender, rentalAssetId, rental.price, rental.duration);
-    }
+        _checkOwnerAndAvailability(msg.sender, rental);
 
-    function unlistAsset(uint256 rentalAssetId) external {
+        rental.status = RentalStatus.Unavailable;
+
+        vault.unlockAsset(msg.sender, rental.assetId, 1);
+
         emit RentalAssetUnlisted(msg.sender, rentalAssetId);
     }
 
     function updateRental(uint256 rentalAssetId, uint256 price, uint256 duration) external {
-        emit RentalAssetUpdated(msg.sender, rentalAssetId, price, duration);
+        RentalAsset storage rental = rentalAssets[rentalAssetId];
+
+        _checkOwnerAndAvailability(msg.sender, rental);
+
+        rental.price = price;
+        rental.duration = duration;
+
+        emit RentalAssetUpdated(msg.sender, rentalAssetId);
     }
+
+    ///////////////////////////////////////////////////////////
+    ///                   RENTER FUNCTIONS                  ///
+    ///////////////////////////////////////////////////////////
 
     function rentAsset(uint256 rentalAssetId) external {
-        RentalAsset memory rental = rentals[rentalAssetId];
+        RentalAsset storage rental = rentalAssets[rentalAssetId];
 
-        emit RentalAssetRented(msg.sender, rentalAssetId, rental.price, rental.duration, block.timestamp);
+        if (rental.status != RentalStatus.Available) {
+            revert AssetRentalNotAvailable(rental.status);
+        }
+
+        vault.lockAsset(msg.sender, igcTokenId, rental.price);
+        vault.unlockAsset(rental.owner, igcTokenId, rental.price);
+
+        rental.status = RentalStatus.Rented;
+        rental.renter = msg.sender;
+        rental.expiration = block.timestamp + rental.duration;
+
+        rentedAssets[msg.sender][rental.assetId]++;
+
+        emit RentalAssetRented(msg.sender, rentalAssetId, block.timestamp);
     }
 
-    function checkRentalStatus(uint256 rentalAssetId) external view returns (RentalStatus status) {
-        return rentals[rentalAssetId].status;
+    function checkRentalStatus(uint256 rentalAssetId) external returns (bool) {
+        RentalAsset storage rental = rentalAssets[rentalAssetId];
+
+        if (block.timestamp >= rental.expiration) {
+            rentedAssets[rental.renter][rental.assetId]--;
+
+            rental.status = RentalStatus.Available;
+            rental.renter = address(0);
+            rental.expiration = 0;
+
+            emit RentalAssetRelisted(rental.owner, rentalAssetId);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    ///////////////////////////////////////////////////////////
+    ///                INTERNAL FUNCTIONS                   ///
+    ///////////////////////////////////////////////////////////
+
+    function _checkOwnerAndAvailability(address user, RentalAsset storage rentalAsset) internal view {
+        if (rentalAsset.status != RentalStatus.Available) {
+            revert AssetRentalNotAvailable(rentalAsset.status);
+        }
+
+        if (rentalAsset.owner != user) {
+            revert AssetRentalNotTheOwner(user, rentalAsset.owner);
+        }
     }
 
     ///////////////////////////////////////////////////////////
@@ -164,76 +217,27 @@ contract AssetRental is IERC1155Receiver {
     /// @notice Get the rental asset details.
     /// @param rentalAssetId The ID of the rental asset.
     /// @return rentalAsset The rental asset details.
-    function getRentalAsset(uint256 rentalAssetId) external view returns (RentalAsset memory rentalAsset) {
-        return rentals[rentalAssetId];
-    }
-
-    /// @notice Get the rental asset owner.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return owner The rental asset owner.
-    function getRentalAssetOwner(uint256 rentalAssetId) external view returns (address owner) {
-        return rentals[rentalAssetId].owner;
-    }
-
-    /// @notice Get the rental asset renter.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return renter The rental asset renter.
-    function getRentalAssetRenter(uint256 rentalAssetId) external view returns (address renter) {
-        return rentals[rentalAssetId].renter;
-    }
-
-    /// @notice Get the rental asset asset ID.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return assetId the rental asset asset ID.
-    function getRentalAssetTokenId(uint256 rentalAssetId) external view returns (uint256 assetId) {
-        return rentals[rentalAssetId].assetId;
-    }
-
-    /// @notice Get the rental asset price.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return price The rental asset price.
-    function getRentalAssetPrice(uint256 rentalAssetId) external view returns (uint256 price) {
-        return rentals[rentalAssetId].price;
-    }
-
-    /// @notice Get the rental asset duration.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return duration The rental asset duration.
-    function getRentalAssetDuration(uint256 rentalAssetId) external view returns (uint256 duration) {
-        return rentals[rentalAssetId].duration;
-    }
-
-    /// @notice Get the rental asset expiration.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return expiration The rental asset expiration.
-    function getRentalAssetExpiration(uint256 rentalAssetId) external view returns (uint256 expiration) {
-        return rentals[rentalAssetId].expiration;
-    }
-
-    /// @notice Get the rental asset status.
-    /// @param rentalAssetId The ID of the rental asset.
-    /// @return status The rental asset status.
-    function getRentalAssetStatus(uint256 rentalAssetId) external view returns (RentalStatus status) {
-        return rentals[rentalAssetId].status;
+    function getRentalAsset(uint256 rentalAssetId) public view returns (RentalAsset memory rentalAsset) {
+        return rentalAssets[rentalAssetId];
     }
 
     /// @notice Get the rented asset balance of a user.
     /// @param user The address of the user.
     /// @param assetId The ID of the asset.
     /// @return balance The rented asset balance of the user.
-    function getRentedAsset(address user, uint256 assetId) external view returns (uint256 balance) {
+    function getRentedAssetBalance(address user, uint256 assetId) public view returns (uint256 balance) {
         return rentedAssets[user][assetId];
     }
 
     /// @notice Get the rental asset count.
     /// @return count The rental asset count.
-    function getRentalCount() external view returns (uint256 count) {
+    function getRentalAssetCount() public view returns (uint256 count) {
         return rentalAssetCount;
     }
 
     /// @notice Get the IGC token ID.
     /// @return assetId The IGC token ID.
-    function getIGCTokenId() external view returns (uint8 assetId) {
+    function getIGCTokenId() public view returns (uint8 assetId) {
         return igcTokenId;
     }
 
